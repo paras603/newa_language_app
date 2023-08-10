00667ab4c91205e21da61175d5007479700fabaa
paras603 <parasmanirai1@gmail.com>
2023-08-10 13:01:13 +0545

    added voice read functionality

--- app/src/main/java/com/mastercoding/newarilanguage/MainActivity.kt
+++ app/src/main/java/com/mastercoding/newarilanguage/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.mastercoding.newarilanguage
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Media
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun SayTheDialog(view : View){
+        val clickedBtn : Button = view as Button
+
+        var mediaPlayer : MediaPlayer = MediaPlayer.create(
+            this,
+            resources.getIdentifier(
+                clickedBtn.tag.toString(),
+                "raw",
+                packageName
+            )
+        )
+        mediaPlayer.start()
+    }
+
 }
